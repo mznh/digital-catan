0f96b3cdb1833908382886c2b1a2e8c3ae6b964b
mznh <hiromtym@gmail.com>
2021-04-15 19:55:55 +0900

    hw-observer側mockを整備

--- hw-observer/src/main.py
+++ hw-observer/src/main.py
@@ -1,4 +1,27 @@
 #!./bin/python3
 # coding: utf-8
 
+from websocket import create_connection
+import json
+
 print("this is temporary code")
+
+ws = create_connection("ws://localhost:8888")
+print("Sending 'Hello, World'...")
+
+## テストメッセージ
+## 3番目に道コマを置く
+tmp_data = {
+   'type': 'put road koma',
+   'target': 'test target',
+   'value': 3,
+   "message": "test message from python code"
+}
+
+ws.send(json.dumps(tmp_data))
+print("Sent")
+print("Receiving...")
+result =  ws.recv()
+print("Received '%s'" % result)
+
+ws.close()
