94cc8777efee0a19e536ed85177a81112c60c3a2
jityoudou <kotobuki8hon@gmail.com>
2021-09-19 23:27:54 +0900

    道コマ・家コマエフェクト・背景の画像を差し替え配置を調整

--- tile-view/src/app/model/graphics.ts
+++ tile-view/src/app/model/graphics.ts
@@ -18,40 +18,40 @@ export class GraphicBook{
 // アニメーションの小回り情報もここ
 // GraphicBookと一対一対応
 export const GRAPHIC_DATA ={
-  ROAD_KOMA: new Graphic("assets/pic/test_road.png", new AnimationInfo(10,10,5,2,192,192)),
-  SETTLEMENT_KOMA: new Graphic("assets/pic/test_settlement.png", new AnimationInfo(10,10,5,2,192,192)),
+  ROAD_KOMA: new Graphic("assets/pic/effect_road_red.png", new AnimationInfo(10,10,10,1,480,240)),
+  SETTLEMENT_KOMA: new Graphic("assets/pic/effect_koma_blue.png", new AnimationInfo(10,10,10,1,480,480)),
   CITY_KOMA: new Graphic("assets/pic/test_road.png"),
   THIEF_KOMA: new Graphic("assets/pic/test_road.png"),
   TEST_TREASURE: new Graphic("assets/pic/test_treasure.png", new AnimationInfo(10,10,5,2,192,192)),
-  TEST_BACKGROUND: new Graphic("assets/pic/test_back.png", new AnimationInfo(4,10,4,1,480,480)),
+  TEST_BACKGROUND: new Graphic("assets/pic/background_pasture.png", new AnimationInfo(1,100,1,1,480,480)),
 };
 
 /// 以下細々した定数
 //ここの数値は完全に仮ぎめ
 //後々必ず精査すること
-export const GRAPHIC_POSITION ={
-  ROAD_KOMA:[
-    new Point(200,  45, 5.0*M_PI/3.0),
-    new Point(390, 110, 0.0*M_PI/3.0),
-    new Point(420, 320, 1.0*M_PI/3.0),
-    new Point(280, 440, 2.0*M_PI/3.0),
-    new Point(90, 370, 3.0*M_PI/3.0),
-    new Point(55, 170, 4.0 * M_PI / 3.0),
+export const GRAPHIC_POSITION = {
+  ROAD_KOMA: [
+    new Point(480, 200, 3.0 * M_PI / 3.0),
+    new Point(395, 428, 4.0 * M_PI / 3.0),
+    new Point(155, 468, 5.0 * M_PI / 3.0),
+    new Point(0, 280, 0.0 * M_PI / 3.0),
+    new Point(85, 52, 1.0 * M_PI / 3.0),
+    new Point(325, 12, 2.0 * M_PI / 3.0),
   ],
-  SETTLEMENT_KOMA:[
-    new Point(200,  45, 5.0*M_PI/3.0),
-    new Point(390, 110, 0.0*M_PI/3.0),
-    new Point(420, 320, 1.0*M_PI/3.0),
-    new Point(280, 440, 2.0*M_PI/3.0),
-    new Point(90, 370, 3.0*M_PI/3.0),
-    new Point(55, 170, 4.0 * M_PI / 3.0),
+  SETTLEMENT_KOMA: [
+    new Point(52, 85, 5.0 * M_PI / 3.0),
+    new Point(280, 0, 0.0 * M_PI / 3.0),
+    new Point(468, 154, 1.0 * M_PI / 3.0),
+    new Point(428, 395, 2.0 * M_PI / 3.0),
+    new Point(200, 480, 3.0 * M_PI / 3.0),
+    new Point(12, 325, 4.0 * M_PI / 3.0),
   ],
-  CITY_KOMA:[
-    new Point(200,  45, 5.0*M_PI/3.0),
-    new Point(390, 110, 0.0*M_PI/3.0),
-    new Point(420, 320, 1.0*M_PI/3.0),
-    new Point(280, 440, 2.0*M_PI/3.0),
-    new Point(90, 370, 3.0*M_PI/3.0),
+  CITY_KOMA: [
+    new Point(200, 45, 5.0 * M_PI / 3.0),
+    new Point(390, 110, 0.0 * M_PI / 3.0),
+    new Point(420, 320, 1.0 * M_PI / 3.0),
+    new Point(280, 440, 2.0 * M_PI / 3.0),
+    new Point(90, 370, 3.0 * M_PI / 3.0),
     new Point(55, 170, 4.0 * M_PI / 3.0),
   ],
   THIEF_KOMA: new Point(200,  45, 5.0*M_PI/3.0),
